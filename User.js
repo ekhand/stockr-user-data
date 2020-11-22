588a0162e3f92f7f2e5348380809ab09522a0c17
ekhand <ekhand@live.unc.edu>
2020-11-22 13:53:46 -0500

    fin

--- User.js
+++ User.js
@@ -1,16 +1,45 @@
+const user_data = require('data-store')({path: process.cwd() + '/data/user.json'});
+
 class User {
 
-    constructor (username, first, last, email) {
+    constructor (username, pass, first, last, email, stocks) {
         this.username = username;
+        this.pass = pass;
         this.first = first;
         this.last = last;
         this.email = email;
-        this.stocks = [];
+        this.stocks = stocks;
     }
 }
 
 User.getUsernames = () => {
     //return array of all usernames
+    return Object.keys(user_data.data);
 }
+
+User.findByUsername = (username) => {
+    //finds user by username
+    let udata = user_data.get(username);
+    if(udata == null) {
+        return null;
+    }
+    else {
+        return new User(udata.username, udata.pass, udata.first, udata.last, udata.email, udata.stocks);
+    }
+}
+
+User.create = (username, pass, first, last, email) => {
+    //creates new user
+    let u = new User(username, pass, first, last, email, []);
+    user_data.set(username, u);
+    return u;
+}
+
+User.update = (username, userObj) => {
+    user_data.set(username, userObj);
+}
+
+let u1 = new User("ekhand", "comp426", "Eli", "Handwerk", "ekhand@live.unc.edu");
+user_data.set(u1.username, u1)
 
 module.exports = User;
--- index.js
+++ index.js
@@ -1,16 +1,75 @@
 const express = require('express');
-
 const app = express();
 
 const User = require('./User.js');
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 app.get('/user', (req, res) => {
-    res.json(User.getUsernames);
+    res.json(User.getUsernames());
     return;
 });
 
-const port = 3030;
+app.get('/user/:username', (req, res) => {
+    let u = User.findByUsername(req.params.username);
+    if (u == null) {
+        res.status(404).send('User does not exist');
+    }
+    else {
+        res.json(u);
+    }
+    return;
+})
 
+app.post('/user', (req, res) => {
+    let {username, pass, first, last, email} = req.body;
+
+    if(User.findByUsername(username) != null) {
+        res.status(500).send("Username is already taken!");
+    }
+    else {
+        let u = User.create(username, pass, first, last, email);
+        res.json(u);
+    }
+    return;
+})
+
+app.put('/user/:username', (req, res) => {
+    let u = User.findByUsername(req.params.username);
+
+    if(u == null) {
+        res.status(404).send('User does not exist');
+    }
+    else {
+        let {action, stock} = req.body;
+        if(action === 'push') {
+            let index = u.stocks.indexOf(stock);
+            if(index <= -1) {
+                u.stocks.push(stock);
+            }
+            else {
+                res.status(500).send('User is already following such stock');
+            }
+        }
+        else {
+            let index = u.stocks.indexOf(stock);
+            if(index > -1) {
+                u.stocks.splice(index, 1);
+            }
+            else {
+                res.status(404).send('User does not follow such stock');
+                return;
+            }
+        }
+
+        User.update(req.params.username, u);
+        res.json(User.findByUsername(req.params.username));
+    }
+    return;
+})
+
+const port = 3030;
 app.listen(port, () => {
     console.log("User data API is running on port " + port);
 })
